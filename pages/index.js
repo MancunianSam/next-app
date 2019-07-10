8540e88c45e3d8c4912ce98954f8c3da44c4932c
Sam Palmer <github@sampalmer.dev>
2019-07-10 09:19:42 +0100

    Add authentication HOC for pages

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,26 @@ import React from "react";
 import Link from "next/link";
 import Nav from "../components/nav";
 import Head from "next/head";
+import { WithAuth } from "../auth";
 
-const Home = () => (
-  <div>
-    <Nav />
-    <Head>
-      <link
-        href="https://s3.eu-west-2.amazonaws.com/assets.tdr.tna/assets/gov.css"
-        rel="stylesheet"
-      />
-    </Head>
+const Home = props => {
+  console.log(props)
+  return (
+    < div >
+      <Nav />
+      <Head>
+        <link
+          href="https://s3.eu-west-2.amazonaws.com/assets.tdr.tna/assets/gov.css"
+          rel="stylesheet"
+        />
+      </Head>
 
-    <div className="hero">
-      <h1 className="title">Welcome to TDR</h1>
-    </div>
-  </div>
-);
+      <div className="hero">
+        <h1 className="title">Welcome to TDR</h1>
+      </div>
+    </div >
+  )
+};
 
 
-export default Home;
+export default WithAuth(Home);
